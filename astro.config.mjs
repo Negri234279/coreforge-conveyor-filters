3860e548d82873004846db03df6f31e647e2f283
Negri234279 <rafalejo01@gmail.com>
2026-05-12 20:13:47 +0200

    feat: actualizar configuración de metadatos y mejorar gestión de imágenes OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://coreforge-conveyor-filters.negri.es',
     output: 'server',
     adapter: node({ mode: 'standalone' }),
     vite: {
